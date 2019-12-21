0195bc378e01a41737897c86dbcfd64cce3c8710
= <=>
2019-12-21 19:09:13 +0530

    menu change

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -4,38 +4,49 @@
   </div>
 
   <div class="clearfix"></div>
-  <br />
-
   <!-- sidebar menu -->
   <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
     <div class="menu_section">
       <!-- <h3>General</h3> -->
       <ul class="nav side-menu">
 
-        <li><a href="{{url('/home')}}"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+        <li><a href="{{ url('home') }}"><i class="fa fa-home"></i> Dashboard</a></li>
+        <li><a href="{{ url('/') }}"><i class="fa fa-dashboard"></i> Firm Master</a></li>
         <!-- User Management Sidebar -->
         @if(auth()->user()->hasPermission('view.roles') || auth()->user()->hasPermission('view.users'))
-					<li><a><i class="fa fa-users"></i>User Management <span class="fa fa-chevron-down"></span></a>
-						<ul class="nav child_menu">
-							@permission('view.roles')
-							<li><a href="{{url('role')}}"> Role / Permission</a></li>
-							@endpermission
-							@permission('view.users')
-							<li><a href="{{url('user')}}"> User</a></li>
-							@endpermission
-						</ul>
-					</li>
-				@endif
-        <!-- <li><a href="{{url('/user')}}"><i class="fa fa-user"></i> User</a></li> -->
-        <li><a href="{{url('/city')}}"><i class="fa fa-building"></i> City</a></li>
-        <li><a href="{{url('/dealer')}}"><i class="fa fa-users"></i> Dealer</a></li>
-        <li><a href="{{url('/category')}}"><i class="fa fa-list"></i> Category</a></li>
-        <li><a href="{{url('/product')}}"><i class="fa fa-th-large"></i> Product</a></li>
-        <li><a href="#"><i class="fa fa-user"></i> Party Master</a></li>
-        <li><a href="{{url('/order')}}"><i class="fa fa-shopping-cart"></i> Order</a></li>
-        <li><a href="#"><i class="fa fa-user"></i> Stock Adjustment/Purchase</a></li>
-        <li><a href="{{url('/settings')}}"><i class="fa fa-cog"></i> Settings</a></li>
-        <li><a href="#"><i class="fa fa-user"></i> Report</a></li>
+            <li><a><i class="fa fa-users"></i>User Management <span class="fa fa-chevron-down"></span></a>
+                <ul class="nav child_menu">
+                    @permission('view.roles')
+                    <li><a href="{{url('role')}}"> Role / Permission</a></li>
+                    @endpermission
+                    @permission('view.users')
+                    <li><a href="{{url('user')}}"> User</a></li>
+                    @endpermission
+                </ul>
+            </li>
+        @endif
+        <li><a><i class="fa fa-arrows"></i>Masters <span class="fa fa-chevron-down"></span></a>
+            <ul class="nav child_menu">
+                <li><a href="{{ url('state') }}"> State Master</a></li>
+                <li><a href="{{ url('state') }}"> City Master</a></li>
+                <li><a href="{{ url('dealer') }}"> Party Master</a></li>
+                <li><a href="{{ url('category') }}"> Category</a></li>
+                <li><a href="{{ url('product') }}"> Product</a></li>
+            </ul>
+        </li>
+
+        <li><a href="{{url('order')}}"><i class="fa fa-shopping-cart"></i> Orders</a></li>
+        <li><a href="#"><i class="fa fa-line-chart"></i> Stock Adjustment / Purchase</a></li>
+
+        <li><a><i class="fa fa-bar-chart"></i>Reports <span class="fa fa-chevron-down"></span></a>
+            <ul class="nav child_menu">
+                <li><a href="{{ url('/') }}"> Party Wise Report</a></li>
+                <li><a href="{{ url('/') }}"> Product Wise Report</a></li>
+                <li><a href="{{ url('/') }}"> Date Wise Report</a></li>
+                <li><a href="{{ url('/') }}"> City Wise Report</a></li>
+            </ul>
+        </li>
+        <li><a href="{{url('settings')}}"><i class="fa fa-cog"></i> Settings</a></li>
       </ul>
     </div>
   </div>
